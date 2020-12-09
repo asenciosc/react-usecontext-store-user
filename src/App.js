7ca7e1245c314096347e374ada24d6c157fcf984
Chris <asenciosc@outlook.com>
2020-12-08 18:59:36 -0700

    added useContext hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Index } from "./pages";
+import { About } from "./pages/about";
+import { UserContext } from "./UserContext";
 
-function App() {
+function AppRouter() {
+  const [user, setUser] = useState(null);
+
+  // prevents  value from changing unless value, setValue changes
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <UserContext.Provider value={value}>
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+        </UserContext.Provider>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default AppRouter;
---
+++ src/utils/login.js
@@ -0,0 +1,7 @@
+export const login = async () => {
+  return {
+    id: 4,
+    username: "johndoe",
+    email: "johndoe@email.com",
+  };
+};
